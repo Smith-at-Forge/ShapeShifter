9c5b128bdbeb3a3b708969a40dd3bb072a5108c6
Peter <70228941+PK39@users.noreply.github.com>
2025-02-10 19:12:31 +0100

    added Pause Screen
    Pause Screen with Options: Fortsetzen, Neustarten, HauptMenue

---
+++ Assets/Scripts/Core/PausenMenue.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PausenMenue : MonoBehaviour
+{
+    public GameObject pausenMenueUI;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausenMenueUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausenMenueUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    
+}
