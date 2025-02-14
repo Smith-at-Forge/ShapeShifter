eb9a5e75bca54919c7ee6d8a05e77c30962fd920
Thejustchill <janplevy@gmail.com>
2025-02-14 22:04:53 +0100

    Update
    Level 1 Finalisiert soweit möglich
    Logik GegnerAktivierung Fledermaus programmiert + im Prefab abgespeichert

--- Assets/Scripts/Jan-Test/Gegneraktivierung.cs
+++ Assets/Scripts/Jan-Test/Gegneraktivierung.cs
@@ -5,22 +5,34 @@ public class GegnerAktivierung : MonoBehaviour
     public float aktivierungsDistanz = 5f; // Distanz, bei der die Fledermaus aktiv wird
     private Animator animator;
 	public Transform player;
-
+	public EnemyPatrol enemyPatrolScript;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("player").transform;
         animator = GetComponent<Animator>();
+		enemyPatrolScript = GetComponent<EnemyPatrol>();
+		
+		if (enemyPatrolScript != null)
+        {
+            enemyPatrolScript.enabled = false;
+        }
     }
 
     void Update()
     {
+		
         // Überprüfung, ob der Spieler in der Nähe ist
         if (player != null && Vector3.Distance(transform.position, player.position) < aktivierungsDistanz)
         {
-            if (animator != null && !animator.enabled) 
+		// Aktiviere EnemyPatrol, falls es noch nicht aktiv ist
+            if (enemyPatrolScript != null && !enemyPatrolScript.enabled)
             {
-                animator.enabled = true;
+                enemyPatrolScript.enabled = true;
+                if (animator != null && !animator.enabled)
+                {
+                    animator.enabled = true;
+                }
             }
         }
     }
